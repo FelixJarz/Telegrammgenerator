2ab7c31c0da4762f7859ea9aa3e4559a3d571f5e
Jonas Haase <jonas.b.haase@gmail.com>
2023-07-21 13:11:31 +0200

    many changes, singleton class

--- WAMAS_Telegrammgenerator/src/package_background/SessionData_Singleton.java
+++ WAMAS_Telegrammgenerator/src/package_background/SessionData_Singleton.java
@@ -1,29 +1,40 @@
 package package_background;
 
+import java.io.File;
+
 public class SessionData_Singleton {
 	
 	static final String ROOT_DIR = "C:\\Telegrammgenerator";
 	static final String PROJECT_FOLDER = ROOT_DIR + "\\Projects";
 	static final String OUTPUT_FOLDER = ROOT_DIR + "\\Output";
-	static private SessionData_Singleton selectedProject = null;
+	
+	static private SessionData_Singleton instance = null;
+	private String selectedProject;
+	
+	
 	
 	private SessionData_Singleton(){
     }
 
 	public static synchronized SessionData_Singleton getInstance(){
 
-        if (selectedProject == null)
-            selectedProject = new SessionData_Singleton();
+        if (instance == null) {
+        	instance = new SessionData_Singleton();
+        }
 
-        return selectedProject;
+        return instance;
     }	
 	
-	public void setSelectedProject() {
-		
+	public void setSelectedProject(String selProject) {
+		selectedProject = selProject;
 	}
 	
-	public void getSelectedproject() {
-		
+	public String getSelectedProject() {
+		return selectedProject;
+	}
+	
+	public String getSelectedProjectPath() {
+		return PROJECT_FOLDER + File.separator + selectedProject;
 	}
 	
 }
