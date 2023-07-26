000ce679bdc73c130574d665a4d2f30bdf579128
Jonas Haase <jonas.b.haase@gmail.com>
2023-07-26 12:09:44 +0200

    new classes

---
+++ WAMAS_Telegrammgenerator/src/package_background/RecordType.java
@@ -0,0 +1,12 @@
+package package_background;
+
+//Map statt Liste bei Masterrecord Types
+//Beginn bis End Attributes extra funktion 
+//Überall kleine funktionen schreiben 
+
+public class RecordType {
+	
+	public RecordType(String name) { //needs more arguments 
+		
+	}
+}
