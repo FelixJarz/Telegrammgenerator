7f21189ceed4393d9cf6da92c39fda407afc07f7
HaaseJonas <jonas.b.haase@gmail.com>
2024-01-24 23:39:17 +0100

    bit more compact

--- WAMAS_Telegrammgenerator/src/package_background/FileContentReader.java
+++ WAMAS_Telegrammgenerator/src/package_background/FileContentReader.java
@@ -98,9 +98,9 @@ public class FileContentReader {
 			while((line = br.readLine()) != null) {
 				String[] values = line.split(";"); 
 				System.out.println("Description: " + values[1] + " | " + "Shortname: " + values[4] + " | " + "Mandatory: " + values[8]);
-				if(values[1].equals("Description") == false) {descriptions.add(values[1]);}
-				if(values[1].equals("Description") == false) {shortnames.add(values[4]);}
 				if(values[1].equals("Description") == false) {
+					descriptions.add(values[1]);
+					shortnames.add(values[4]);
 					if(values[8].equals("X")) {
 						mandatoryCells.add(values[1]);
 					}else {
